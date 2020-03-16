35fc9dd23fd04d35c902a87c3a9a3fab4ac83495
mvkras <avkras1992@gmail.com>
2020-03-16 13:53:41 +1000

    Testing Python, simple

---
+++ Test2.py
@@ -0,0 +1,23 @@
+print('Hello, 47, welcome back to work')
+print('Hello, 47, welcome back to work')
+print('Hello, 47, welcome back to work')
+print('Hello, 47, welcome back to work')
+print('Hello, 47, welcome back to work')
+print('Hello, 47, welcome back to work')
+print('Hello, 47, welcome back to work')
+print('Hello, 47, welcome back to work')
+print('Hello, 47, welcome back to work')
+print('Hello, 47, welcome back to work')
+
+name = 'mvkras'
+age=27
+profile = 'Phyton'
+
+print('{0} мне {1} лет и я обожаю - {2}.'.format(name, age, profile))
+
+a = 'python'
+b = 'is'
+c = 'excellent'
+
+d = a[0] + c [0] + a[len(a)-1]+b
+print(d)    # Вывод будет - пенис
---
+++ Test_file.py
@@ -0,0 +1,28 @@
+print('Я буду магистром Phyton')
+print('Я буду магистром Phyton')
+print('Я буду магистром Phyton')
+print('Я буду магистром Phyton')
+print('Я буду магистром Phyton')
+print('Я буду магистром Phyton')
+print('Я буду магистром Phyton')
+print('Я буду магистром Phyton')
+print('Я буду магистром Phyton')
+print('Я буду магистром Phyton')
+
+mvkras = 25 + 75
+print(mvkras)
+a = 'Отпуск начался'
+print(a)
+if 4 > 2:
+    print(5)
+else :
+    print(11)
+b = 'Я тут!'
+print(b)
+
+x = "Операция сложения:"
+y = 14+15
+z = 32
+print(x, (y+z))
+
+
